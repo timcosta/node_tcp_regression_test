fdbc4e52b8a69f56f72685e8a983b384868cb1b1
Tim Costa <tim@timcosta.io>
2018-12-11 22:39:18 -0500

    travis matrix

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const Http = require('http');
 const Hapi = require('hapi');
-const server = new Hapi.Server();
 
-server.connection({port: 8000});
+if (process.env.SERVER === 'http') {
+	const app = new Http.Server();
 
-server.start(() => {
-	console.log('Running on 8000');
-});
+	app.on('request', (req, res) => {
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.write('Not Found');
+		res.end('\n');
+	});
+
+	app.listen(8000, () => {
+		console.log(`http running on port 8000`);
+	});
+} else {
+	const server = new Hapi.Server();
+
+	server.connection({port: 8000});
+
+	server.start(() => {
+		console.log('hapi running on 8000');
+	});
+}
